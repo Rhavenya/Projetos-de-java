4f374ac5b8b27720c0fbb5ccd45d4301755874f0
Rhavenya <rhavenyalima@gmail.com>
2025-04-21 17:42:22 -0300

    Atividade de java!

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Conversão de Celsius para Farenheight");
+        double valor1 = 30.4;
+        System.out.println("O valor em graus celsius é: " + valor1);
+        double resultado = ((valor1 * 1.8) + 32);
+        System.out.println("O valor em Farenheight será: " + resultado);
+    }
+}
